ea2f9f4bb22c6bf45bebbe6921880d771b15880d
mmteejay <mmteejay@live.com>
2022-09-08 14:36:06 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#includ <stdio.h>
+ /**
+  * Programming is like building a
+  * multilingual puzzle
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
