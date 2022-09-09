5c16f11c51df3dec8da3f55b040919c3e30ac09f
mmteejay <mmteejay@live.com>
2022-09-09 07:34:30 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
 /*puts*/
 
 
-int main(void)
+int main(void) 
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
