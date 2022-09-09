97ceb17ab14a5cafd520f05135895dc172d96566
mmteejay <mmteejay@live.com>
2022-09-09 07:47:00 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-/*puts*/
+/**
+ * main - print the strng in the put function
+ *
+ * Description: using the main function
+ * this program prints "programmjng is like building a multilingual puzz
+ * return: 0
+ */
 
 
 int main(void)
