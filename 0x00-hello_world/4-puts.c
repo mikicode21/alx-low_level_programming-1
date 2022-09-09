44f39af6b87e519aeacde37e8be5522e2a6091d0
mmteejay <mmteejay@live.com>
2022-09-09 07:08:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-/* program */
 
-int main(void) 
-	
 
- {
-	puts("\"Programming is like building a multilingual puzzle");
+/* use function puts*/
 
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle");
 	return(0);
-}
+
