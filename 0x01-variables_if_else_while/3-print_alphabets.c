7aa35db17082a695a39d0d0e2cbdb4b0cdbfc2ae
mmteejay <mmteejay@live.com>
2022-09-11 11:48:34 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase and uppercase
+ *
+ * Return: 0 always
+ * */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++);
+	{
+		putchar(ch);
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++);
+	{
+		putchar(ch);
+	}
+	return (0);
+}
