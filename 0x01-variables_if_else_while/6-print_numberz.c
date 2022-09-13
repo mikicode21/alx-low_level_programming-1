9a67a796f303d1ac5639cba360cfcc1bcd70ad16
mmteejay <mmteejay@live.com>
2022-09-12 17:52:05 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,12 +6,11 @@
  */
 int main(void)
 {
-	long num;
+	int num;
 
 	for (num = 0; num < 10; num++)
-	{
-		putchar(num % 10) + 0;
-	}
+		putchar((num % 10) + '0');
+
 	putchar('\n');
 	return (0);
 }
