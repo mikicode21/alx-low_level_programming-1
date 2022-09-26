16f7ce07065a025d70f4244fdaf577657aeb4340
mmteejay <mmteejay@live.com>
2022-09-26 14:11:51 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,18 @@
 #include "main.h"
+
 /**
- * _memset - sets memory to array
- * @s: array to set
- * @b: value to set it as
- * @n: n amount of times
- * Return: char value of s
+ * _memset - Entry point
+ * Desc: Entry
+ * @s: pointer
+ * @b: string
+ * @n: number the bytes
+ * Return: Always 0 (Success)
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0 ; i < n ; i++)
 	{
 		s[i] = b;
 	}
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,20 +1,34 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagsums - prints diagonal summaries
- * @a: array to use
- * @size: size of diagonal
+ * print_diagsums - Entry point
+ * Desc: Entry
+ * @a: pointer
+ * @size: value
+ * Return: Always 0 (Success)
  */
 void print_diagsums(int *a, int size)
 {
 	int i;
-	int diag0 = 0
-	int diagT = 0;
+	int j;
+	int dg_r = 0;
+	int dg_l = 0;
+	int k;
 
 	for (i = 0; i < size; i++)
 	{
-		diagO += a[(i * size) + i];
-		diagT += a[(size - 1) + ((size - 1) * i)];
+		for (j = 0; j < size; j++)
+		{
+			k = (i * size) + j;
+			if (i == j)
+			{
+				dg_r = dg_r + a[k];
+			}
+			if ((i + j) == (size - 1))
+			{
+				dg_l = dg_l + a[k];
+			}
+		}
 	}
-	printf("%d, %d\n", diagO, diagT);
+	printf("%d, %d\n", dg_r, dg_l);
 }
