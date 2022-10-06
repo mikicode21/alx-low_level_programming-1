3066176cbe21be25234b510f3c3a38024cad8317
mmteejay <mmteejay@live.com>
2022-10-06 14:21:42 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,52 +1,49 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * _strlen - length of a string
- * @s: input char
- * Return: length of a string
- */
-
-int _strlen(char *s)
-{
-int i;
-i = 0;
-while (s[i] != '\0')
-i++;
-return (i);
-}
-/**
- * string_nconcat - change value using
- * @s1: manipulates char1
- * @s2: manipulates char2
- * @n: manipulates integer
- * Return: memory
+ * string_nconcat - concatenates two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ * @n: amount of chars touse of second string
+ *
+ * Return: a pointer to the concatenated string or NULL if the process fails
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int a, b, i;
-char *p;
-if (!s1)
-s1 = "";
-else
-a = _strlen(s1);
-if (!s2)
-s2 = "";
-else
-b = _strlen(s2);
-if (n > b)
-n = b;
-p = malloc((a + n + 1) * sizeof(char));
-if (p == NULL)
-return (NULL);
-for (i = 0; i < a; i++)
-{
-p[i] = s1[i];
-}
-for (; i < (a + n); i++)
-{
-p[i] = s2[i - a];
-}
-p[i] = '\0';
-return (p);
+	unsigned int i_s1, i_s2, len1, len2;
+	char *a;
+
+	if (s1 == 0)
+		s1 = "";
+	for (len1 = 0; s1[len1] != 0; len1++)
+	{
+	}
+	if (s2 == 0)
+		s2 = "";
+	for (len2 = 0; s2[len2] != 0; len2++)
+	{
+	}
+	if (n >= len2)
+		a = malloc(sizeof(char) * (len1 + len2 + 1));
+	else
+		a = malloc(sizeof(char) * (len1 + n + 1));
+	if (a == 0)
+		return (0);
+	for (i_s1 = 0; i_s1 < len1; i_s1++)
+		a[i_s1] = s1[i_s1];
+	if (n >= len2)
+	{
+		for (i_s2 = 0; i_s2 < len2; i_s1++, i_s2++)
+			a[i_s1] = s2[i_s2];
+	}
+	else
+	{
+		for (i_s2 = 0; i_s2 < n; i_s1++, i_s2++)
+			a[i_s1] = s2[i_s2];
+	}
+	a[i_s1] = 0;
+	return (a);
 }
